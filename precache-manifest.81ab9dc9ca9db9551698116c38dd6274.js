71e2ef940026c2a68ee2a8e5f956e5fcd6260439
Brian Juan <43415804+pjuan4603@users.noreply.github.com>
2020-10-02 17:56:46 -0400

    Updates

---
+++ precache-manifest.81ab9dc9ca9db9551698116c38dd6274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5be478a501b8c0ba72da0b6e320d9c4",
+    "url": "/React_practice/robofriends/index.html"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/React_practice/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b4d060ee0627c00721ad",
+    "url": "/React_practice/robofriends/static/css/main.3a3a9d06.chunk.css"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/React_practice/robofriends/static/js/2.2a06c861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_practice/robofriends/static/js/2.2a06c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d060ee0627c00721ad",
+    "url": "/React_practice/robofriends/static/js/main.334f5fd0.chunk.js"
+  },
+  {
+    "revision": "447a54dfca6eb39b3137",
+    "url": "/React_practice/robofriends/static/js/runtime-main.6e85317f.js"
+  }
+]);
